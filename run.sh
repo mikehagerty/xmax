f6ea81b89386ad049d200958e3dbf20c266c62d7
Mike Hagerty <mth@mikes-mac-mini1.bc.edu>
2013-07-23 17:15:27 -0400

    Modified META-INF/MANIFEST.MF - Added '.' to Class-path so that xmax.jar will look in ./ for log4j.properties file.

--- run.sh
+++ run.sh
@@ -1,2 +1,8 @@
-#java -Xms512M -Xmx512M -jar xmax.jar
-java -Xms512M -Xmx512M -jar xmax.jar -d '/Users/mth/mth/ASLData/xs0/seed/*/2012/2012_160_*/00_LH*seed'
+# Simple run (currently will only look for log4j.properties in the current dir e.g., ./log4j.properties )
+#java  -Xms512M -Xmx512M -jar xmax.jar
+
+# Same but give a path to seed files:
+java  -Xms512M -Xmx512M -jar xmax.jar -d '/Users/mth/mth/ASLData/xs0/seed/*/2012/2012_160_*/00_LH*seed'
+
+# Here's a way to explicitly point to a log4j.properties file:
+#java -Dlog4j.configuration=file:./src/log4j.properties -Xms512M -Xmx512M -jar xmax.jar -d '/Users/mth/mth/ASLData/xs0/seed/*/2012/2012_160_*/00_LH*seed'
